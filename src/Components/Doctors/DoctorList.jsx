8bba0a6fa3d0d7d7efea6577b6849ee395db425b
ShyamaAgrawal <shyama.agrawal162@gmail.com>
2024-07-17 00:55:09 +0530

    Doctor's Dashboard

--- src/Components/Doctors/DoctorList.jsx
+++ src/Components/Doctors/DoctorList.jsx
@@ -1,13 +1,27 @@
-import { doctors } from '../../assets/data/doctor.js'
-import DoctorCard from './DoctorCard'
-import '../CSS/Doctors.css'
+import DoctorCard from "./DoctorCard";
+import "../CSS/Doctors.css";
+import { BASE_URL } from "../../../config.js";
+import useFetchData from "../../hooks/useFetchData.jsx";
+import Error from "../Error/Error.jsx";
+import Loading from "../Loader/Loading.jsx";
 
 const DoctorList = () => {
-    return (
-        <div className="doctor-list">
-            {doctors.map((doctor)=> <DoctorCard key={doctor.id} doctor={doctor}/>)}
-        </div>
-    )
-}
+    const { data: doctors, loading, error } = useFetchData(`${BASE_URL}/doctors`);
+    // console.log(doctors)
 
-export default DoctorList
+  return (
+    <>
+      {loading && !error && <Loading />}
+      {!loading && error && <Error />}
+      {!loading && !error && (
+        <div className="doctor-list">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor._id} doctor={doctor} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DoctorList;
--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,26 +1,63 @@
-import { doctors } from '../../assets/data/doctor.js'
-import DoctorCard from '../../Components/Doctors/DoctorCard'
-import Testimonial from '../../Components/Testimonial/Testimonial.jsx'
-import '../PagesCSS.css'
+import DoctorCard from "../../Components/Doctors/DoctorCard";
+import Testimonial from "../../Components/Testimonial/Testimonial.jsx";
+import "../PagesCSS.css";
+import { BASE_URL } from "../../../config.js";
+import useFetchData from "../../hooks/useFetchData.jsx";
+import Error from "../../Components/Error/Error.jsx";
+import Loading from "../../Components/Loader/Loading.jsx";
+import { useEffect, useState } from "react";
 
 const Doctors = () => {
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+  
+  const handleSearch = () => {
+    setQuery(query.trim());
+    console.log("handling query")
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedQuery(query);
+    },500)
+  
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [query])
+  
+  const {
+    data: doctors,
+    loading,
+    error,
+  } = useFetchData(`${BASE_URL}/doctors?query=${debouncedQuery}`);
+
+
   return (
     <>
       <section className="bg-[#fff9ea]">
         <div className="container text-center">
           <h2 className="heading">Find a Doctor</h2>
           <div className="find-doc">
-            <input type="search" className="inp" placeholder='Search Doctor' />
-            <button className='btn mt-0 rounded-[0px] rounded-r-[8px]'>Search</button>
+            <input type="search" className="inp" value={query} onChange={(e)=>setQuery(e.target.value)} placeholder="Search doctor by name or specification" />
+            <button className="btn mt-0 rounded-[0px] rounded-r-[8px]" onClick={handleSearch}>
+              Search
+            </button>
           </div>
         </div>
       </section>
 
       <section>
         <div className="container w-full lg:w-[85%] mx-auto">
-          <div className="doctor-list" style={{ marginTop: 0}}>
-            {doctors.map((doctor) => <DoctorCard key={doctor.id} doctor={doctor} />)}
-          </div>
+          {loading && !error && <Loading />}
+          {!loading && error && <Error />}
+          {!loading && !error && (
+            <div className="doctor-list" style={{ marginTop: 0 }}>
+              {doctors.map((doctor) => (
+                <DoctorCard key={doctor._id} doctor={doctor} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
 
@@ -29,17 +66,18 @@ const Doctors = () => {
           <div className="testimonial lg:w-[85%] lg:mx-auto">
             <div className="testimonial-top">
               <h2 className="heading text-center">Our great doctors</h2>
-              <p className="text__para text-center" style={{ color: ' gray' }}>World-class care for everyone. Our health system offers unmatched,
-                expert health care.</p>
+              <p className="text__para text-center" style={{ color: " gray" }}>
+                World-class care for everyone. Our health system offers
+                unmatched, expert health care.
+              </p>
             </div>
 
             <Testimonial />
-
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
